26b7079ff1a748f53178393cfd9e8eb43ca25124
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2026-04-12 12:48:22 +1000

    jaxb version and plugin updated, moved to jakarta. It compiles but utils are all broken now so can use it and all GML duplicated and no longer in its own package which is atrocious. Tried everything nothing works to keep it separate, consideer downloading xsds of gml locally and then generate in stages instead or something.

--- src/main/java/org/goplanit/xml/bindings/TimeAdapter.java
+++ src/main/java/org/goplanit/xml/bindings/TimeAdapter.java
@@ -1,6 +1,7 @@
 package org.goplanit.xml.bindings;
 
-import javax.xml.bind.annotation.adapters.XmlAdapter;
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
